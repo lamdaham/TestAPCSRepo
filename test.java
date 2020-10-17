6da1420843b1d66002760d29b224503d5715e229
lamdaham <ilam21@stuy.edu>
2020-10-16 20:35:48 -0400

    I tested this program and it works now

---
+++ test.java
@@ -0,0 +1,10 @@
+public class test {
+	public static void main(String[] args) {
+		for (int i = 1; i <= 3; i++) {
+			for (int j = 1; j<=4-i; j++) {
+				System.out.print(i);
+			}
+			System.out.println();
+		}
+	}
+}
