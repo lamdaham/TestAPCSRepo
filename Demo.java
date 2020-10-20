ceca47ba015fe388bab9d48366d7aecc8479c791
lamdaham <ilam21@stuy.edu>
2020-10-20 19:40:53 -0400

    aiyaaaa more coding stuff; arr to string hw; 10/20/20

--- Demo.java
+++ Demo.java
@@ -1,4 +1,55 @@
+import java.util.Random;
+
 public class Demo {
+
+
+	public static String arrToString(int[]arr) {
+		String output = "{";
+ 		for (int i = 0; i<arr.length; i++) {
+    		output+= arr[i];
+    		if (i<arr.length-1) {
+      			output += ", ";
+    		}
+  		}
+  		output += "}";
+  		return output;
+	}
+
+
+	public static String arrayDeepToString(int[][]arr) {
+		String output = "{";
+		for (int i = 0; i<=arr.length-2; i++) {
+			output += arrToString(arr[i]);
+			output += ", ";
+		}
+		output += arrToString(arr[arr.length-1])+"}";
+		return output;
+	}
+
+	public static int[][] create2DArray(int rows, int cols, int maxValue) {
+		int[][] output = new int[rows][cols];
+		Random random = new Random();
+		for (int r = 0; r<rows; r++) {
+			for (int c = 0; c<cols; c++) {
+				output[r][c] = random.nextInt(maxValue);
+			}
+		}
+		return output;
+	}
+
+
+	public static int[][] create2DArrayRandomized(int rows, int cols, int maxValue) {
+		Random random = new Random();
+		int[][] output = new int[rows][];
+		for (int r = 0; r <rows; r++) {
+			output[r] = new int[random.nextInt(cols)];
+			for (int c = 0; c < output[r].length; c++) {
+				output[r][c] = random.nextInt(maxValue);
+			}
+		}
+		return output;
+	}
+
 	public static void main(String[] args) {
 		int input = 0;
 		if (args.length == 0) {
